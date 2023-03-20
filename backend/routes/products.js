d89e2b598b3adeacad26c065b23bf573664bb41d
Nicholas Peters <peters.nicholas@gmail.com>
2023-03-20 14:21:07 +0100

    add 'find product by id' endpoint

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -2,27 +2,42 @@ const express = require("express");
 const router = express.Router();
 const Product = require("../models/product-model");
 
-router.get("/", function (req, res, next) {
+router.get("/", async function (req, res, next) {
   // Show all products
+  try {
+    const products = await Product.find();
+    console.log(products);
+    res.json(products);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-router.get("/:id", function (req, res, next) {
+router.get("/:id", async function (req, res, next) {
   // Show single product, by id
+  try {
+    const product = await Product.findById(req.params.id);
+    res.json(product);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 router.post("/add", function (req, res, next) {
-  // Add a new product with name, price, description, lager
-  const product = new Product({
-    name: req.body.name,
-    price: req.body.price,
-    description: req.body.description,
-    lager: req.body.lager,
-  });
+  try {
+    const product = new Product({
+      name: req.body.name,
+      price: req.body.price,
+      description: req.body.description,
+      lager: req.body.lager,
+    });
 
-  product.save();
+    product.save();
 
-  res.json(product);
-  console.log(product);
+    res.json(product);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,48 +4,62 @@ const User = require("../models/user-model");
 const CryptoJS = require("crypto-js");
 
 router.get("/", async function (req, res, next) {
-  const users = await User.find().select("-password");
-  res.json(users);
+  try {
+    const users = await User.find().select("-password");
+    res.json(users);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 router.post("/", async function (req, res, next) {
-  const userId = req.body;
-  const allUsers = await User.find();
+  try {
+    const userId = req.body;
+    const allUsers = await User.find();
 
-  const foundUser = allUsers.find((user) => user._id === userId);
+    const foundUser = allUsers.find((user) => user._id === userId);
 
-  res.json(foundUser);
+    res.json(foundUser);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 router.post("/add", function (req, res, next) {
-  const user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: CryptoJS.AES.encrypt(
-      req.body.password,
-      process.env.SALT
-    ).toString(),
-  });
+  try {
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: CryptoJS.AES.encrypt(
+        req.body.password,
+        process.env.SALT
+      ).toString(),
+    });
 
-  user.save();
+    user.save();
 
-  res.json(user);
+    res.json(user);
+  } catch (error) {}
 });
 
 router.post("/login", async function (req, res, next) {
-  const user = await User.findOne({ email: req.body.email });
-  if (user) {
-    const decryptedPassword = CryptoJS.AES.decrypt(
-      user.password,
-      process.env.SALT
-    ).toString(CryptoJS.enc.Utf8);
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      const decryptedPassword = CryptoJS.AES.decrypt(
+        user.password,
+        process.env.SALT
+      ).toString(CryptoJS.enc.Utf8);
 
-    if (decryptedPassword === req.body.password) {
-      user.isLoggedIn = true;
-      res.json(user);
-    } else {
-      res.json({ message: "Wrong password" });
+      if (decryptedPassword === req.body.password) {
+        user.isLoggedIn = true;
+        res.json(user);
+      } else {
+        res.json({ message: "Wrong password" });
+      }
     }
+  } catch (error) {
+    console.log(error);
   }
 });
 
