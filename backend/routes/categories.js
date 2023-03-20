562783023f33c23d7195b414e11a7b06efd37605
Nicholas Peters <peters.nicholas@gmail.com>
2023-03-20 16:51:31 +0100

    Add category route and model. add 'add new category' endpoint

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../models/category-model");
 
-router.get("/", function (req, res, next) {
-  res.send("Categories");
+router.post("/add", function (req, res, next) {
+  const category = new Category({
+    name: req.body.name,
+  });
+  const token = req.body.token;
+
+  if (!token) {
+    return res.status(401).json({ message: "Token is required" });
+  }
+
+  category.save();
+  res.json(category);
 });
 
 module.exports = router;
