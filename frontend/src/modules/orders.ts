5f235283ac5747828cd2dfdc4a419c82d93378d7
Nicholas Peters <peters.nicholas@gmail.com>
2023-03-24 13:03:46 +0100

    refactor orderlist functions

--- frontend/src/modules/orders.ts
+++ frontend/src/modules/orders.ts
@@ -35,37 +35,40 @@ export function renderOrderCard() {
     document.getElementById('backBtn')?.addEventListener('click', () => {
         renderProductCard();
     });
-    renderOrderList(allUsers, allProducts);
+    renderOrderList(allProducts);
 }
 
-// TODO: fix products display
-async function renderOrderList(allUsers: any, allProducts: { _id: string; name: string; }[]) {
+async function renderOrderList(allProducts: { _id: string; name: string; }[]) {
     const orders = await getOrders();
-    const orderList = document.getElementById('order-list') as HTMLDivElement;
     const userEmail = localStorage.getItem('email');
     const user: string = allUsers.find((user: { email: string | null; }) => user.email === userEmail)._id;
     
     orders.forEach((order: IOrder) => {
         if (order.user === user) {
-            let usersOrderedProducts: string[] = [];
-            let productNames: string[] = [];
-            
-            order.products.forEach((product: { productId: string; }) => {
-                usersOrderedProducts.push(product.productId);
-            });            
-            
-            usersOrderedProducts.forEach((productId: string) => {
-                allProducts.forEach((product: { _id: string; name: string; }) => {
-                    if (product._id == productId) {                        
-                        productNames.push(product.name);
-                    }
-                });
-            });
-            
-            const orderItem = `
-                <div class="order-item">Products ordered: ${productNames}</div>
-            `;
-            orderList.innerHTML += orderItem;
+            renderProductNames(order, allProducts);
         }}
     );
+}
+
+function renderProductNames(order: IOrder, allProducts: { _id: string; name: string; }[]) {
+    const orderList = document.getElementById('order-list') as HTMLDivElement;
+    let usersOrderedProducts: string[] = [];
+    let productNames: string[] = [];
+
+    order.products.forEach((product: { productId: string; }) => {
+        usersOrderedProducts.push(product.productId);
+    });
+
+    usersOrderedProducts.forEach((productId: string) => {
+        allProducts.forEach((product: { _id: string; name: string; }) => {
+            if (product._id == productId) {
+                productNames.push(product.name);
+            }
+        });
+    });
+
+    const orderItem = `
+                <div class="order-item">Products ordered: ${productNames}</div>
+            `;
+    orderList.innerHTML += orderItem;
 }
